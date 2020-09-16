bd671ab0fd7f58b3ad5da3000b697d1eb6fcc15f
José Ignacio <josignaciot@gmail.com>
2020-09-16 00:25:38 -0300

    Fixing the 90-degree problem for the clock hands do not back up to their original position. Adding 3 base variables.

--- clock/app.js
+++ clock/app.js
@@ -2,23 +2,29 @@ const secondHand = document.querySelector(".second-hand");
 const minHand = document.querySelector(".min-hand");
 const hourHand = document.querySelector(".hour-hand");
 
+secondsBase = 0;
+minutesBase = 0;
+hoursBase = 0;
+
 function setDate() {
   const now = new Date();
   const seconds = now.getSeconds();
-  console.log(seconds);
-  const secondDegrees = (seconds / 60) * 360 + 90;
-  console.log(secondDegrees);
+  const secondDegrees = (seconds / 60) * 360 + 90 + secondsBase;
   secondHand.style.transform = `rotate(${secondDegrees}deg)`;
 
+  if (seconds == 59) secondsBase += 360;
+
   const minutes = now.getMinutes();
-  console.log(minutes);
-  const minutesDegrees = (minutes / 60) * 360 + 90;
+  const minutesDegrees = (minutes / 60) * 360 + 90 + minutesBase;
   minHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
+  if (minutes == 59) minutesBase += 360;
+
   const hours = now.getHours();
-  console.log(hours);
-  const hoursDegrees = (hours / 24) * 360 + 90;
+  const hoursDegrees = (hours / 24) * 360 + 90 + hoursBase;
   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+  if (hours == 11) hoursBase += 360;
 }
 
 setInterval(setDate, 1000);
